1588e413ee9645ee186f12e34ac47bc622162c03
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2017-05-22 03:44:54 -0700

    Changes

--- finder.js
+++ finder.js
@@ -19,4 +19,133 @@ angular.module('finderApp', [])
       console.log(finder.locations);
     };
 
+  })
+  .controller('ProductsController', function($scope) {
+    var products = this;
+
+    products.products = {
+      1: {
+        name: 'Jet Blak',
+        image: 'jet_blak.png',
+        available: true,
+        banner: 'jet_blak_large.jpg',
+        description: 'Our unique blend of all organic, fair-trade & shade grown coffee beans roasted to perfection and cold-brewed in a highly purified alkaline water.'
+      },
+      2: {
+        name: 'Blak Chocolate',
+        image: 'blak_chocolate.png',
+        available: true,
+        banner: 'blak_chocolate_large.jpg',
+        description: 'Our Jet Blak Koffee topped  with all natural essence of cacao & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
+      },
+      3: {
+        name: 'Vanilla Bean',
+        image: 'vanilla_bean.png',
+        available: false,
+        banner: 'vanilla_bean_large.jpg',
+        description: 'Our Jet Blak Koffee topped with an organic vanilla extract & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
+      },
+      4: {
+        name: 'Sweet Chilli Bean',
+        image: 'sweet_chilli_bean.png',
+        available: false,
+        banner: 'sweet_chilli_bean_large.jpg',
+        description: 'Our Jet blak Koffee spiced w/Cayenne, flavored with an organic Orange essential oil & sweetened w/all natural, non-artificial sugar-free sweeteners.'
+      },
+      5: {
+        name: 'Chai Pai',
+        image: 'chai_pai.png',
+        available: false,
+        banner: 'chai_pai_large.jpg',
+        description: 'Our Jet Blak Koffee blended with a Turmeric-Chai spice blend & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
+      },
+      6: {
+        name: 'Mint Mocha',
+        image: 'mint_mocha.png',
+        available: false,
+        banner: 'mint_mocha_large.jpg',
+        description: 'Our Blak Chocolate Koffee topped with an organic peppermint essential oil.'
+      },
+      7: {
+        name: 'Blue Chamomoile',
+        image: 'blue_chamomoile.png',
+        available: false,
+        banner: 'blue_chamomoile_large.jpg',
+        description: 'Our Jet BlaK Koffee brewed w/Chamomile flowers, a natural hazelnut extract & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
+      },
+      8: {
+        name: 'Blak Rose',
+        image: 'blak_rose.png',
+        available: false,
+        banner: 'blak_rose_large.jpg',
+        description: 'Our Vanilla Bean Koffee mixed with a High Vibration Bulgarian Rose Water.'
+      }
+    };
+    products.overlay = 0;
+
+    products.show = function(product) {
+      products.overlay = 1;
+      products.active = product;
+      var background = "url('images/products/" + product.banner + "') #fff";
+      $(".products__overlay").css("background", background);
+      $(".products__overlay").fadeIn();
+    };
+
+    products.hide = function() {
+      products.overlay = 0;
+      $(".products__overlay").fadeOut();
+    };
+
   });
+
+  
+  // products.products = {
+  //   1: {
+  //     name: 'Blak Chocolate',
+  //     image: 'blak_chocolate.png',
+  //     available: true,
+  //     banner: 'blak_chocolate_large.jpg',
+  //   },
+  //   2: {
+  //     name: 'Blak Chocolate',
+  //     image: 'blak_chocolate.png',
+  //     available: true,
+  //     banner: 'blak_chocolate_large.jpg',
+  //   },
+  //   3: {
+  //     name: 'Blak Chocolate',
+  //     image: 'blak_chocolate.png',
+  //     available: true,
+  //     banner: 'blak_chocolate_large.jpg',
+  //   },
+  //   4: {
+  //     name: 'Blak Chocolate',
+  //     image: 'blak_chocolate.png',
+  //     available: true,
+  //     banner: 'blak_chocolate_large.jpg',
+  //   },
+  //   5: {
+  //     name: 'Blak Chocolate',
+  //     image: 'blak_chocolate.png',
+  //     available: true,
+  //     banner: 'blak_chocolate_large.jpg',
+  //   },
+  //   6: {
+  //     name: 'Blak Chocolate',
+  //     image: 'blak_chocolate.png',
+  //     available: true,
+  //     banner: 'blak_chocolate_large.jpg',
+  //   },
+  //   7: {
+  //     name: 'Blak Chocolate',
+  //     image: 'blak_chocolate.png',
+  //     available: true,
+  //     banner: 'blak_chocolate_large.jpg',
+  //   },
+  //   8: {
+  //     name: 'Blak Chocolate',
+  //     image: 'blak_chocolate.png',
+  //     available: true,
+  //     banner: 'blak_chocolate_large.jpg',
+  //   }
+  // };
---
+++ products.js
@@ -0,0 +1,56 @@
+// angular.module('finderApp', [])
+//   .controller('ProductsController', function($scope) {
+//     var products = this;
+
+//     products.products = {
+//       1: {
+//         name: 'Blak Chocolate',
+//         image: 'blak_chocolate.png',
+//         available: true,
+//         banner: 'blak_chocolate.jpg',
+//       },
+//       2: {
+//         name: 'Blak Chocolate',
+//         image: 'blak_chocolate.png',
+//         available: true,
+//         banner: 'blak_chocolate.jpg',
+//       },
+//       3: {
+//         name: 'Blak Chocolate',
+//         image: 'blak_chocolate.png',
+//         available: true,
+//         banner: 'blak_chocolate.jpg',
+//       },
+//       4: {
+//         name: 'Blak Chocolate',
+//         image: 'blak_chocolate.png',
+//         available: true,
+//         banner: 'blak_chocolate.jpg',
+//       },
+//       5: {
+//         name: 'Blak Chocolate',
+//         image: 'blak_chocolate.png',
+//         available: true,
+//         banner: 'blak_chocolate.jpg',
+//       },
+//       6: {
+//         name: 'Blak Chocolate',
+//         image: 'blak_chocolate.png',
+//         available: true,
+//         banner: 'blak_chocolate.jpg',
+//       },
+//       7: {
+//         name: 'Blak Chocolate',
+//         image: 'blak_chocolate.png',
+//         available: true,
+//         banner: 'blak_chocolate.jpg',
+//       },
+//       8: {
+//         name: 'Blak Chocolate',
+//         image: 'blak_chocolate.png',
+//         available: true,
+//         banner: 'blak_chocolate.jpg',
+//       }
+//     }
+
+//   });
