5b39928f539dbd2c092205513ab3e53ec5c6c87f
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2017-05-26 18:56:16 -0700

    Remove products

--- finder.js
+++ finder.js
@@ -45,41 +45,41 @@ angular.module('finderApp', [])
         banner: 'vanilla_bean_large.jpg',
         description: 'Our Jet Blak Koffee topped with an organic vanilla extract & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
       },
-      4: {
-        name: 'Sweet Chilli Bean',
-        image: 'sweet_chilli_bean.png',
-        available: false,
-        banner: 'sweet_chilli_bean_large.jpg',
-        description: 'Our Jet blak Koffee spiced w/Cayenne, flavored with an organic Orange essential oil & sweetened w/all natural, non-artificial sugar-free sweeteners.'
-      },
-      5: {
-        name: 'Chai Pai',
-        image: 'chai_pai.png',
-        available: false,
-        banner: 'chai_pai_large.jpg',
-        description: 'Our Jet Blak Koffee blended with a Turmeric-Chai spice blend & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
-      },
-      6: {
-        name: 'Mint Mocha',
-        image: 'mint_mocha.png',
-        available: false,
-        banner: 'mint_mocha_large.jpg',
-        description: 'Our Blak Chocolate Koffee topped with an organic peppermint essential oil.'
-      },
-      7: {
-        name: 'Blue Chamomoile',
-        image: 'blue_chamomoile.png',
-        available: false,
-        banner: 'blue_chamomoile_large.jpg',
-        description: 'Our Jet BlaK Koffee brewed w/Chamomile flowers, a natural hazelnut extract & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
-      },
-      8: {
-        name: 'Blak Rose',
-        image: 'blak_rose.png',
-        available: false,
-        banner: 'blak_rose_large.jpg',
-        description: 'Our Vanilla Bean Koffee mixed with a High Vibration Bulgarian Rose Water.'
-      }
+      // 4: {
+      //   name: 'Sweet Chilli Bean',
+      //   image: 'sweet_chilli_bean.png',
+      //   available: false,
+      //   banner: 'sweet_chilli_bean_large.jpg',
+      //   description: 'Our Jet blak Koffee spiced w/Cayenne, flavored with an organic Orange essential oil & sweetened w/all natural, non-artificial sugar-free sweeteners.'
+      // },
+      // 5: {
+      //   name: 'Chai Pai',
+      //   image: 'chai_pai.png',
+      //   available: false,
+      //   banner: 'chai_pai_large.jpg',
+      //   description: 'Our Jet Blak Koffee blended with a Turmeric-Chai spice blend & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
+      // },
+      // 6: {
+      //   name: 'Mint Mocha',
+      //   image: 'mint_mocha.png',
+      //   available: false,
+      //   banner: 'mint_mocha_large.jpg',
+      //   description: 'Our Blak Chocolate Koffee topped with an organic peppermint essential oil.'
+      // },
+      // 7: {
+      //   name: 'Blue Chamomoile',
+      //   image: 'blue_chamomoile.png',
+      //   available: false,
+      //   banner: 'blue_chamomoile_large.jpg',
+      //   description: 'Our Jet BlaK Koffee brewed w/Chamomile flowers, a natural hazelnut extract & sweetened with a 100% all natural, non-artificial sugar-free sweeteners.'
+      // },
+      // 8: {
+      //   name: 'Blak Rose',
+      //   image: 'blak_rose.png',
+      //   available: false,
+      //   banner: 'blak_rose_large.jpg',
+      //   description: 'Our Vanilla Bean Koffee mixed with a High Vibration Bulgarian Rose Water.'
+      // }
     };
     products.overlay = 0;
 
